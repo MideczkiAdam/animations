863e71f7bc9d8acbe624789b58109cc9a54861d1
MideczkiAdam <2008adika@gmail.com>
2026-04-21 14:01:11 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import EmojiWrapper from './components/Emoji/EmojiWrapper'
 import AnimatedBorder from './components/AnimatedBorder/AnimatedBorder'
 import Revolver from './components/RevolverMenu/Revolver'
+import AnimatedTitle from './components/AnimatedTitle/AnimatedTitle'
+import Hammer from './components/Hammer/Hammer'
 
 const App = () => {
   return (
     <div style={{display: "flex", flexDirection: "column", gap: "20px"}}>
       <EmojiWrapper/>
       <AnimatedBorder/>
-      <Revolver />
+      {/* <Revolver /> */}
+      {/* <AnimatedTitle titles={[
+        {name:"Hello", bgColor: "red"},
+        {name:"World", bgColor: "blue"},
+        {name:"asdas", bgColor: "green"}
+        ]}/> */}
+      <Hammer />
     </div>
   )
 }
